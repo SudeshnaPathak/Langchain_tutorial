5a1ddec695b0422e60192730428cd8236e8ae1ef
Subhojit Guin <subhojitguin2004@gmail.com>
2024-09-29 14:48:01 +0530

    Updated text prompt template

--- SQL_ChatBot/prompts.py
+++ SQL_ChatBot/prompts.py
@@ -8,28 +8,27 @@ load_dotenv()
 
 
 example_prompt = ChatPromptTemplate.from_messages(
-     [
-         ("human", "{input}\nSQLQuery:"),
-         ("ai", "{query}"),
-     ]
+    [
+        ("human", "{input}\nSQLQuery:"),
+        ("ai", "{query}"),
+    ]
  )
 
 
 few_shot_prompt = FewShotChatMessagePromptTemplate(
-     example_prompt=example_prompt,
-     example_selector=get_example_selector(),
-     input_variables=["input","top_k"],
+    example_prompt=example_prompt,
+    example_selector=get_example_selector(),
+    input_variables=["input","top_k"],
  )
 
 
 final_prompt = ChatPromptTemplate.from_messages(
-     [
-         ("system", "You are a MySQL expert. Given an input question, create a syntactically correct MySQL query to run. Unless otherwise specificed.\nHere are the tables info : {table_info}\nHere are the tables that you can refer to : {selected_tables}\n\nBelow are a number of examples of questions and their corresponding SQL queries. Those examples are just for reference and should be considered while answering follow up questions.In case of 2 or more tables with same coloumn name ,while displaying the details , if asked for , you should display it from any one of the tables without any ambiguity. Answer only from the Database and not from anywhere else.Include all the column names and table names within backticks which generating the SQL queries for correct execution"),
-         few_shot_prompt,
-         MessagesPlaceholder(variable_name="messages"),
-         ("human", "{input}"),
-     ]
-      
+    [
+        ("system", "You are a MySQL expert. Given an input question, create a syntactically correct MySQL query to run. Unless otherwise specificed.\nHere are the tables info : {table_info}\nHere are the tables that you can refer to : {selected_tables}\n\nBelow are a number of examples of questions and their corresponding SQL queries. Those examples are just for reference and should be considered while answering follow up questions.In case of 2 or more tables with same coloumn name ,while displaying the details , if asked for , you should display it from any one of the tables without any ambiguity. Answer only from the Database and not from anywhere else.Include all the column names and table names within backticks which generating the SQL queries for correct execution"),
+        few_shot_prompt,
+        MessagesPlaceholder(variable_name="messages"),
+        ("human", "{input}"),
+    ]  
  )
 
 question_prompt = ChatPromptTemplate.from_template(""" You are an expert assistant in reframing questions. If the user's question specifically asks for a report, reframe the question to include the following details: the detailed NOC Guidelines, definitions of some technical terms related to groundwater, and generalized Training Opportunities related to Groundwater.
